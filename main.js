6f1289d3f9012757735d003fa39bb3c91784e419
kfunkin <32571927+kfunkin@users.noreply.github.com>
2019-09-13 09:14:14 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+let menuBtn = document.querySelector('#menu-icon');
+let mainMenu = document.querySelector('#menu');
+let closeBtn = document.querySelector('#close-btn');
+
+menuBtn.addEventListener('click', function() {
+  mainMenu.style.height = "100%";
+  mainMenu.style.opacity = "0.95";
+  closeBtn.style.visibility = "visible";
+  closeBtn.style.opacity = "1";
+})
+
+closeBtn.addEventListener('click', function() {
+  mainMenu.style.height = "0";
+  mainMenu.style.opacity = "0";
+  closeBtn.style.opacity = "0";
+  closeBtn.style.visibility = "hidden";
+})
